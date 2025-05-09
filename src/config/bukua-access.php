6b6ef71222659d691a5eee491242537e0917629e
digram <dennismayeku@gmail.com>
2025-05-10 02:00:47 +0300

    fix:settings path

--- src/config/bukua-access.php
+++ src/config/bukua-access.php
@@ -2,8 +2,8 @@
 
 return [
     'bukua_access' => [
-        'bukua_access_client_id'     => env('BUKUA_ACCESS_CLIENT_ID'),
-        'bukua_access_client_secret' => env('BUKUA_ACCESS_CLIENT_SECRET'),
-        'base_url'                   => env('BUKUA_BASE_URL', 'https://bukua-core.apptempest.com/'),
+        'client_id'     => env('BUKUA_ACCESS_CLIENT_ID'),
+        'client_secret' => env('BUKUA_ACCESS_CLIENT_SECRET'),
+        'base_url'      => env('BUKUA_BASE_URL', 'https://bukua-core.apptempest.com/'),
     ],
 ];
